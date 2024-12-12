422e394d4b4f0b6784a79202ba4a9dcc5b3a8a10
Aman Kumar <amankumar.m99@gmail.com>
2024-12-12 21:32:55 +0530

    spring initializr code

---
+++ src/main/java/m99/bookmyseat/BookMySeatApplication.java
@@ -0,0 +1,13 @@
+package m99.bookmyseat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookMySeatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookMySeatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/m99/bookmyseat/BookMySeatApplicationTests.java
@@ -0,0 +1,13 @@
+package m99.bookmyseat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookMySeatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

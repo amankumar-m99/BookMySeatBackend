4003855e43c602bf40f2b12d2e736243eb7be30f
Aman Kumar <amankumar.m99@gmail.com>
2024-12-24 15:57:38 +0530

    added a helper method

--- src/main/java/m99/bookmyseat/entity/BaseEntity.java
+++ src/main/java/m99/bookmyseat/entity/BaseEntity.java
@@ -1,5 +1,9 @@
 package m99.bookmyseat.entity;
 
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -19,4 +23,11 @@ public class BaseEntity {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+
+	public static List<Long> getIdsFromList(List<? extends BaseEntity> entities){
+		if(entities == null || entities.size() == 0) {
+			return Collections.emptyList();
+		}
+		return entities.stream().map(e -> e.getId()).collect(Collectors.toList());
+	}
 }

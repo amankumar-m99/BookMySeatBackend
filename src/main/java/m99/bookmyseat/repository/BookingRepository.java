d78492c7025b52f68a085e3ad7d0deb6a228fde6
Aman Kumar <amankumar.m99@gmail.com>
2025-01-07 18:13:37 +0530

    corrected the query

--- src/main/java/m99/bookmyseat/repository/BookingRepository.java
+++ src/main/java/m99/bookmyseat/repository/BookingRepository.java
@@ -20,7 +20,7 @@ public interface BookingRepository extends JpaRepository<Booking, Long>{
 	@Query("SELECT b FROM Booking b ORDER BY b.bookingDate DESC")
 	List<Booking> findByUserIdDesc(Long userId);
 	
-	@Query("SELECT b FROM Booking b WHERE b.user.id = :userId and b.showtime.date >= :date")
+	@Query("SELECT b FROM Booking b WHERE b.user.id = :userId and b.showtime.date >= :date ORDER BY b.showtime.date")
 	List<Booking> findUpcomingByUserId(Long userId, Date date);
 
 	@Query("SELECT b FROM Booking b WHERE b.user.id = :userId and b.showtime.date < :date")
